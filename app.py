0cad60ddcd0d2f32367cc688f192415a38ba1a62
umeshkumar <umeshvrv7310@gmail.com>
2025-11-25 18:35:10 +0530

    new

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from application import app
 
 if __name__ == '__main__':
     from application.routes import *
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
 
 
